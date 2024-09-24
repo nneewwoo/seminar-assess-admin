5e636975d79bfa1bef88ee69c0c0d99e39d71320
Owen Garbosa <garbosa.owen@student.capsu.edu.ph>
2024-09-24 23:44:11 +0800

    add for tna and test

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,91 @@
+<script lang="ts">
+	type Question = {
+		question: string;
+		answers: string[];
+		correctAnswer: string;
+	};
+
+	let questions = [] as Question[];
+
+	let newQuestion: Question = { question: '', answers: [], correctAnswer: '' };
+
+	let newQuestionString = '';
+
+	let newAnswer = '';
+
+	const addAnswer = () => {
+		newQuestion.answers = [...newQuestion.answers, newAnswer];
+		newAnswer = '';
+	};
+
+	const addQuestion = () => {
+		newQuestion.question = newQuestionString;
+		newQuestion.correctAnswer = '';
+		questions = [...questions, newQuestion];
+		newQuestion = { question: '', answers: [], correctAnswer: '' };
+	};
+</script>
+
 <br />
 <h1>Test</h1>
+
+<div class="w-full flex" style=" padding: 1rem; ">
+	<div class="w-1/2">
+		<input
+			class="rounded-md"
+			type="text"
+			name="question"
+			bind:value={newQuestionString}
+			placeholder="Enter Question"
+		/>
+		<input
+			class="rounded-md"
+			type="text"
+			name="add-answer"
+			bind:value={newAnswer}
+			placeholder="Enter Options"
+		/>
+		<input
+			class="rounded-md"
+			type="text"
+			name="correct-answer"
+			bind:value={newQuestion.correctAnswer}
+			placeholder="Enter correct answer"
+		/>
+		{#if newQuestion.answers.length > 0}
+			{#each newQuestion.answers as answer, i}
+				<div>
+					<!-- svelte-ignore a11y-label-has-associated-control -->
+					<label>{answer}</label>
+				</div>
+			{/each}
+		{/if}
+		<br />
+		<br />
+		<button class="btn btn-sm variant-ghost-surface" type="button" on:click={addAnswer}
+			>Add option</button
+		>
+		<button class="btn btn-sm variant-ghost-surface" type="button" on:click={addQuestion}
+			>Add Question</button
+		>
+	</div>
+	<div>
+		<h1>QUESTIONS</h1>
+		{#if questions.length > 0}
+			{#each questions as question, i}
+				<div>
+					<h2>{question.question}</h2>
+					{#each question.answers as answer, i}
+						<div>
+							<!-- svelte-ignore a11y-label-has-associated-control -->
+							<label>{answer}</label>
+						</div>
+					{/each}
+				</div>
+			{/each}
+		{/if}
+	</div>
+</div>
 
 <style>
 	h1 {
--- src/routes/tna/+page.svelte
+++ src/routes/tna/+page.svelte
@@ -1,13 +1,67 @@
+<script lang="ts">
+	type Question = {
+		question: string;
+		answers: string[];
+		correctAnswer: string;
+	};
+
+	let questions = [] as Question[];
+
+	let newQuestion: Question = { question: '', answers: [], correctAnswer: '' };
+
+	let newQuestionString = '';
+
+	const addQuestion = () => {
+		newQuestion.question = newQuestionString;
+		newQuestion.correctAnswer = '';
+		questions = [...questions, newQuestion];
+		newQuestion = { question: '', answers: [], correctAnswer: '' };
+	};
+</script>
+
 <br />
 <h1>TNA</h1>
-<p>
-	Topic Preferences Rate the topics from 1 to 5 base of your level of interest. ( 1 is the most
-	interested and 5 is the least of interested )
-</p>
 
-<div style="display: flex; justify-content: flex-end; gap: 1rem;">
-	<button class="btn variant-ghost">Add</button>
-	<button class="btn variant-ghost">Delete</button>
+<div class="w-full flex" style=" padding: 1rem; ">
+	<div class="w-1/2">
+		<input
+			class="rounded-md"
+			type="text"
+			name="question"
+			bind:value={newQuestionString}
+			placeholder="Enter Assessment"
+		/>
+
+		{#if newQuestion.answers.length > 0}
+			{#each newQuestion.answers as answer, i}
+				<div>
+					<!-- svelte-ignore a11y-label-has-associated-control -->
+					<label>{answer}</label>
+				</div>
+			{/each}
+		{/if}
+		<br />
+		<br />
+		<button class="btn btn-sm variant-ghost-surface" type="button" on:click={addQuestion}
+			>Add Assessment</button
+		>
+	</div>
+	<div>
+		<h1>Assessment</h1>
+		{#if questions.length > 0}
+			{#each questions as question, i}
+				<div>
+					<h2>{question.question}</h2>
+					{#each question.answers as answer, i}
+						<div>
+							<!-- svelte-ignore a11y-label-has-associated-control -->
+							<label>{answer}</label>
+						</div>
+					{/each}
+				</div>
+			{/each}
+		{/if}
+	</div>
 </div>
 
 <style>
